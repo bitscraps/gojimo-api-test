57f80a7a1777e91bec36d30466aeed4f1bc9cd55
Graham Hadgraft <graham.hadgraft@gmail.com>
2015-07-28 14:52:49 +0100

    Add icon to attributes saved for a subject
    This appears to be blank for all subjects but could be used later

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,10 +1,11 @@
 class Subject
-  attr_accessor :id, :title, :link, :colour
+  attr_accessor :id, :title, :link, :colour, :icon
 
   def initialize(attributes={})
     @id = attributes[:id] || ""
     @title = attributes[:title] || ""
     @link = attributes[:link] || ""
     @colour = attributes[:colour] || ""
+    @icon = attributes[:icon] || ""
   end
 end
